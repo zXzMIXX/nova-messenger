1b5396b72a4930ac0a3b221f8d7a96d3d28ff855
Loper <7byloper@gmail.com>
2025-09-04 11:15:29 +0300

    Обновление структуры

---
+++ backend/src/main/java/ru/loper/cuantum/CuantumApplication.java
@@ -0,0 +1,13 @@
+package ru.loper.cuantum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuantumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuantumApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/ru/loper/cuantum/CuantumApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.loper.cuantum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuantumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
